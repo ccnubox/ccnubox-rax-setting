6feb7f581f8e24f8dc006829ae34b3dc801618fa
zxc0328 <120797887@qq.com>
2018-08-02 16:39:33 +0800

    feat: init project

---
+++ README.md
@@ -0,0 +1,13 @@
+## ccnubox-iOS-rax-setting
+
+Setting page of ccnubox iOS APP.
+
+### dev
+
+`git submodule init`
+
+`git submodule update`
+
+`npm install`
+
+`npm run start`
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { createElement, Component, render, findDOMNode } from "rax";
+import View from "rax-view";
+
+
+class App extends Component {
+  state = {
+ 
+  };
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        hello
+      </View>
+    );
+  }
+}
+
+export default App;
